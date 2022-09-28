f4dbbd4585cceb5a9712587fcf337284ef658b74
anthony deplanque <anthony.deplanque@csgroup.eu>
2022-09-28 12:57:57 +0200

    added font family variables

---
+++ src/variables/texts/fonts.ts
@@ -0,0 +1,7 @@
+export const FONT_FAMILY: { [name: string]: string } = {
+    ARIAL: "Arial",
+    COMIC_SANS_MS: "Comic Sans MS",
+    VERDANA: "Verdana",
+    SERIF: "serif",
+    SANS_SERIF: "sans-serif",
+};
